5869d3e8aa4ae729eec690eff7fcf3babbee1e1a
Ruben del Olmo <r.olmocalderon@gmail.com>
2020-03-08 16:33:46 +0100

    Html structure refactorized and added tag functionality

--- connect.js
+++ connect.js
@@ -7,9 +7,9 @@ db = new sqlite3.Database('./images.db', (err) => {
     console.log('Connected to the database.');
 });
 
-db.each("SELECT * FROM images;", function(err, row) {                                                                                                                                    
+/*db.each("SELECT * FROM images;", function(err, row) {                                                                                                                                    
     console.log(row);
-});
+});*/
 
 module.exports.createTable = function(query){
     db.run(query,function(err){
@@ -31,7 +31,7 @@ module.exports.close = function(){
     });
 }
 
-module.exports.fetchAll = function(sql,callback){
+var fetchAll = function(sql,callback){
     db.all(sql, [], (err, rows) => {
         if (err) {
             throw err;
@@ -41,7 +41,7 @@ module.exports.fetchAll = function(sql,callback){
     });
 }
 
-module.exports.get = function(sql,callback){
+var get = function(sql,callback){
     db.get(sql, [], (err,row) => {
         if (err) {
             throw err;
@@ -53,7 +53,53 @@ module.exports.get = function(sql,callback){
     });
 }
 
-module.exports.insert = function(item,callback){
+function checkTags(imageId, tags){
+    tags = ['1','2','3','vale-por-','free-phone-calls'];
+    let query = 'SELECT name FROM tags;';
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        
+        rows = rows.map(row => { return row.name; });
+        tags = tags.filter(function(item) {
+            return !rows.includes(item); 
+        });
+
+        if(tags.length > 0){
+            tags.forEach(tag => {
+                insertTag(imageId,tag);
+            });
+        }
+    });
+}
+
+var insertTag = function(imageId,tagName,callback){
+    let query = 'INSERT INTO tags(name)values("' + tagName + '")';
+
+    db.run(query,function(err){
+        if(err){
+            console.log(err.message);
+            return;
+        }
+
+        asociateTag(this.lastID,imageId);
+    });
+}
+
+function asociateTag(tagId,imageId){
+    let query = 'INSERT INTO tagged(tagId,imageId)values("' + tagId +'","' + imageId +'")';
+
+    db.run(query,function(err){
+        if(err){
+            console.log(err.message);
+            return;
+        }
+        console.log("Tag associated;");
+    });
+}
+
+var insert = function(item,callback,tags){
     let query = 'INSERT INTO images(name,size,type,path)values("' + 
         item.name + '", "' + 
         item.size + '", "' + 
@@ -66,6 +112,7 @@ module.exports.insert = function(item,callback){
             return;
         }
         if(callback){
+            checkTags(this.lastID,tags);
             callback(this.lastID,item);
         }
         
@@ -73,7 +120,7 @@ module.exports.insert = function(item,callback){
     });
 }
 
-module.exports.delete = function(id){
+var remove = function(id){
     let query = 'DELETE FROM images WHERE id = "' + id + '";';
     db.run(query,function(err){
         if(err){
@@ -85,3 +132,9 @@ module.exports.delete = function(id){
     })
 
 }
+
+module.exports.get = get;
+module.exports.fetchAll = fetchAll;
+module.exports.insert = insert;
+module.exports.delete = remove;
+module.exports.insertTag = insertTag;
--- create_database.js
+++ create_database.js
@@ -1,7 +1,7 @@
 const connection = require('./connect');
 
 //connection.dropTable('DROP TABLE IF EXISTS images');
-connection.createTable('CREATE TABLE images(' +
+connection.createTable('CREATE TABLE IF NOT EXISTS images(' +
     'id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
     'name text NOT NULL, ' +
     'size text NOT NULL, ' +
@@ -9,5 +9,5 @@ connection.createTable('CREATE TABLE images(' +
     'path text NOT NULL' +
 ');');
 
-connection.createTable('CREATE TABLE tags(id INTEGER PRIMARY KEY AUTOINCREMENT, name text NOT NULL)');
-connection.createTable('CREATE TABLE tagged(tagId INTEGER, imageId INTEGER)');
+connection.createTable('CREATE TABLE IF NOT EXISTS tags(id INTEGER PRIMARY KEY AUTOINCREMENT, name text NOT NULL)');
+connection.createTable('CREATE TABLE IF NOT EXISTS tagged(tagId INTEGER, imageId INTEGER)');
